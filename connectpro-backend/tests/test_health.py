fb6a61260d610f6de2ff771d6e8d98f9f3d6b952
a-dtya <adithyavinod1943@gmail.com>
2025-08-31 20:52:19 +0530

    modified smoke test

--- connectpro-backend/tests/test_health.py
+++ connectpro-backend/tests/test_health.py
@@ -1,6 +1,6 @@
 # tests/test_health.py
 from fastapi.testclient import TestClient #type: ignore
-from main import app  # adjust import if your FastAPI app is in a different file
+from backend.main import app
 
 client = TestClient(app)
 
